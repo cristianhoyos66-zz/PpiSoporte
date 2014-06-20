569ac81ab25c4bfcea2268745ba589ca1bbd5f9f
Juan Felipe Cadavid <jcadavid@intergrupo.com>
2014-06-19 23:26:37 -0500

    Add style to Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,32 @@
 class HomeController < ApplicationController
   def index
+
+  	if current_user.has_role? :admin1ernivel
+  		@requests = Request.where(level: 1)
+  	elsif current_user.has_role? :admin2donivel
+  		@requests = Request.where(level: 2)
+  	elsif current_user.has_role? :admin
+  		@requests = Request.all
+  	elsif current_user.has_role? :requester
+  		@requests = Request.where(user_id: current_user)
+  	else 
+  		flash[:error] = "No tienes permiso para ver este recurso"
+  		redirect_to root_url
+  	end
+    @request_aproved=0
+    @request_rejected=0
+    @request_ended=0
+    @request_n_esta=0
+    @requests.each do |request|
+      if request.status == "Aprobado"
+        @request_aproved=+1
+      elsif request.status == "Rechazado"
+        @request_rejected=+1
+      elsif request.status == "Terminado"
+        @request_ended=+1
+      else
+        @request_n_esta=+1
+      end
+    end  	
   end
 end
